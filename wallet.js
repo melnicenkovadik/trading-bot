2217d9ac9d990873e989a23e4d6519360e148eb8
melnicenkovadik <melnicenkovadik@gmail.com>
2022-08-06 19:29:35 +0200

    fix

--- wallet.js
+++ wallet.js
@@ -4,15 +4,7 @@ var MyWallet = /** @class */ (function () {
     function MyWallet(balance) {
         this.totalProfit = 0;
         this.balance = balance;
-        this.myTradingHistory = [34.69, 31.27, 28.21, 25.67, 23.31,
-            21.21, 19.17, 17.43, 15.86, 14.3,
-            12.99, 11.77, 10.62, 9.66, 8.71,
-            7.85, 7.1, 6.45, 5.86, 5.31,
-            4.82, 4.35, 3.92, 3.54, 3.2,
-            2.89, 2.62, 2.37, 2.15, 1.94,
-            1.75, 1.59, 1.44, 1.31, 1.19,
-            1.08
-        ];
+        this.myTradingHistory = [];
     }
     MyWallet.prototype.getBalance = function () {
         return this.balance;
